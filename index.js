7909b17f0488e43f70513b95b66df161e3a81957
adan.bashir@student.moringaschool.com <adan.bashir@student.moringaschool.com>
2024-04-25 17:37:23 +0300

    Done with lab

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+    for (let i = 0; i < word.length / 2; i++) {
+      const j = word.length - 1 - i;
+      if (word[i] !== word[j]) return false;
+    }
 
-/* 
+    return true;
+}
+/*
   Add your pseudocode here
 */
+// iterate from the beginning to the middle
+// check each letter to the correponding letter from the end
+// if any letters don't match, return false
+// return true
+
+
+
+
 
 /*
   Add written explanation of your solution here
 */
+// means if the word is the same as the word in reverse, I should return true.
+
+// reverse the input string
+
+// if the input is the same as the reversed input
+//   return true
+// else
+//   return false
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
